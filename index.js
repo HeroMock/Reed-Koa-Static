b588e76f8f47621362ceed48f6dd50d29129b264
HansHuang <hys0550@sina.com>
2025-03-23 20:15:18 -0400

    bump dependency package version

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const path = require('path'),
     fs = require('fs'),
     { promisify } = require('util'),
-    send = require('koa-sendfile')
+    { send } = require("@koa/send")
 
 
 module.exports = function serveStatic({ urlPrefix, dirPath, indexPages }) {
@@ -13,27 +13,25 @@ module.exports = function serveStatic({ urlPrefix, dirPath, indexPages }) {
             filePath = filePath.replace(RegExp(`^${urlPrefix}`), '')
         }
 
-        let fullPath = path.join(dirPath, filePath),
+        const fullPath = path.join(dirPath, filePath),
             stats = await promisify(fs.stat)(fullPath).then(stats => stats, () => null)
-
+        console.log(fullPath)
         if (stats && stats.isFile()) {
 
-            await send(ctx, fullPath)
+            await send(ctx, ctx.path, { root: dirPath })
 
         } else if (stats && stats.isDirectory()) {
 
             if (!indexPages || !indexPages.length) {
                 indexPages = ['index.html', 'index.htm']
             }
+            const index = indexPages.findIndex(async s => {
+                const file = path.join(fullPath, s),
+                    indexStats = await promisify(fs.stat)(file)
+                return indexStats && indexStats.isFile()
+            })
 
-            let indexPaths = indexPages.map(s => path.join(fullPath, s)),
-                indexFile = indexPaths.find(async s => {
-                    let indexStats = await promisify(fs.stat)(s)
-                    return indexStats && indexStats.isFile()
-                })
-
-            indexFile ? await send(ctx, indexFile) : await next()
-
+            index > -1 ? await send(ctx, indexPages[index], { root: dirPath }) : await next()
         } else {
             await next()
         }
